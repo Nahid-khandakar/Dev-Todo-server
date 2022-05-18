dd7df3a32dcb4252f9e3d955722c58cb8c4bacbc
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-05-18 19:27:10 +0600

    add post method

--- index.js
+++ index.js
@@ -10,6 +10,33 @@ app.use(express.json())
 // todo - admin
 // DK8M26VX0FZHpJe5
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fjsrw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+    try {
+        await client.connect();
+        const taskCollection = client.db("dev-todo").collection("userTask");
+
+        app.post('/task', async (req, res) => {
+            const taskData = req.body
+            console.log(taskData)
+
+            const result = await taskCollection.insertOne(taskData)
+            return res.send({ success: true, result })
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('Todo server')
 })
